c01f27f6d6db507fbfbd98734bb42b1f6451f879
Pujan Modha <pujanmodha19@gmail.com>
2023-11-05 01:41:23 +0530

    footer responsiveness wip

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -7,7 +7,6 @@ import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faLinkedin, faSquareInstagram, faSquareFacebook} from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
-import {faSquareEnvelope, faSquarePhone} from "@fortawesome/free-solid-svg-icons";
 
 const montserrat = Montserrat({
     subsets: ["latin"],
@@ -16,21 +15,21 @@ const montserrat = Montserrat({
 export default function Footer() {
     return(
         <>
-            <div id="footer" className="bottom-0 mb-0 w-full z-20 min-h-96 text-white">
-               <div className="max-w-7xl mx-auto px-[5vw]">
-                   <div className="grid grid-cols-1 lg:grid-cols-2 text-white text-sm font-medium py-20 lg:py-24">
+            <div id="footer" className="bottom-0 mb-0 w-full z-20 min-h-96 text-white lg:bg-fixed">
+               <div className="max-w-7xl mx-auto px-6 lg:px-8">
+                   <div className="grid grid-cols-1 lg:grid-cols-2 text-white text-sm font-medium py-16 lg:py-24">
                        <div className="flex flex-col">
-                           <div className="flex flex-row mx-auto lg:mx-0 lg:scale-90">
-                               <div className="border-r-2">
+                           <div className="flex flex-col mx-auto lg:mx-0 md:flex-row">
+                               <div className="md:border-r-2">
                                    {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
-                                   <Image src={logo} alt="logo" className="w-72 pr-6"/>
+                                   <Image src={logo} alt="logo" className="w-64 md:w-72 md:pr-6"/>
                                </div>
-                               <div className={`font-sans ${montserrat.variable} grid grid-rows-2 leading-none my-auto pl-6`}>
-                                   <p className="text-[1.7rem] font-medium">Student Branch</p>
-                                   <p className="text-[1.15rem] font-light">Manipal University Jaipur</p>
+                               <div className={`font-sans ${montserrat.variable} grid grid-rows-2 leading-none my-auto md:pl-6`}>
+                                   <p className="text-[2.2rem] md:text-[1.7rem] font-medium">Student Branch</p>
+                                   <p className="text-[1.5rem] md:text-[1.15rem] font-light">Manipal University Jaipur</p>
                                </div>
                            </div>
-                           <div className="flex flex-col lg:flex-row lg:justify-around pt-6 lg:mx-8 lg:scale-90 text-base font-medium space-y-2">
+                           <div className="flex flex-col md:flex-row md:justify-around pt-8 md:pt-6 md:mx-8 md:scale-90 text-base font-medium space-y-2 md:space-y-0">
                                <Link href="https://www.linkedin.com/company/ieeesbmuj/" className="inline-flex"><FontAwesomeIcon icon={faLinkedin} size="xl"/><p className="hover:underline hover:underline-offset-2 pl-1">@ieeesbmuj</p>
                                    <span>
                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
@@ -54,27 +53,27 @@ export default function Footer() {
                                </Link>
                            </div>
                        </div>
-                       <div className="grid lg:grid-cols-2 pt-2">
+                       <div className="grid md:grid-cols-2 pt-2">
                            <div className="flex flex-col">
                                <div className="pt-8 lg:pt-0 lg:pb-4">
                                    <p className="text-xl font-medium pb-2">Contact Us</p>
                                    <ul>
-                                       <li><Link href="tel:+919721490618" className="inline-flex"><FontAwesomeIcon icon={faSquarePhone} size="lg"/><p className="pl-1">+91 97214 90618</p></Link></li>
+                                       <li><Link href="tel:+919721490618"><p>+91 97214 90618</p></Link></li>
                                        <li>Sriansh Raj</li>
                                    </ul>
                                    <br/>
                                    <ul>
-                                       <li><Link href="tel:+919569516125" className="inline-flex"><FontAwesomeIcon icon={faSquarePhone} size="lg"/><p className="pl-1">+91 95695 16125</p></Link></li>
+                                       <li><Link href="tel:+919569516125"><p>+91 95695 16125</p></Link></li>
                                        <li>Atul Raj Sharma</li>
                                    </ul>
                                    <br/>
                                    <ul>
-                                       <li><Link href="mailto:ieee@muj.manipal.edu" className="inline-flex"><FontAwesomeIcon icon={faSquareEnvelope} size="lg"/><p className="pl-1">ieee@muj.manipal.edu</p></Link></li>
+                                       <li><Link href="mailto:ieee@muj.manipal.edu"><p>ieee@muj.manipal.edu</p></Link></li>
                                    </ul>
                                </div>
                                <div className="pt-8 lg:pt-4">
                                    <p className="text-xl font-medium pb-2">Address</p>
-                                   <p className="pr-16 leading-6">Manipal University Jaipur, Dehmi Kalan, Near GVK Toll Plaza, Jaipur-Ajmer Expressway, Jaipur, Rajasthan 303007</p>
+                                   <p className="w-[50vw] md:w-[40vw] lg:w-full lg:pr-16 leading-6">Manipal University Jaipur, Dehmi Kalan, Near GVK Toll Plaza, Jaipur-Ajmer Expressway, Jaipur, Rajasthan 303007</p>
                                </div>
                            </div>
                            <div className="flex flex-col">
