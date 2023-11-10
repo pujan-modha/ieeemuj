f2c9dcea8276eb996d00a9e78c8c4ec646aabe70
Pujan Modha <pujanmodha19@gmail.com>
2023-11-10 09:06:37 +0530

    minor 404 content changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,8 +12,8 @@ export default function NotFound(){
                     <div className="overflow-hidden grid grid-cols-1 md:grid-cols-2 md:w-full pb-8">
                         <div className="text-left">
                             <h1 className="text-4xl text-ieeeblue font-semibold">ERROR!!</h1>
-                            <h1 className="text-9xl font-bold text-ieeeblue">404</h1>
-                            <p className="text-xl lg:text-3xl pb-8 text-ieeedarkblue font-medium">{`Oops!! Looks like you have reached the Jogger's Park.`}</p>
+                            <h1 className="text-9xl font-black text-ieeeblue">404</h1>
+                            <p className="text-xl lg:text-3xl pb-8 text-ieeedarkblue font-medium">{`Looks like you have reached the Jogger's Park.`}</p>
                             <Link href="/"
                                className="px-8 py-3 text-lg font-medium border-2 border-ieeeblue hover:text-white rounded-full hover:bg-ieeeblue text-ieeeblue">GO DOME</Link>
                         </div>
